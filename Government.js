287886563dc870f1ebe787597c66347c77339f5f
Hassib Ashouri <hassib291@hotmail.com>
2019-05-12 22:22:25 -0700

    streamlines\nSome work on blocks class related code is needed

--- Government.js
+++ Government.js
@@ -3,14 +3,45 @@
 let EventEmitter = require('events');
 let Block = require('./block.js');
 let Delegate = require('./Delegate.js');
-let Transaction = require('./transaction.js');
 
-const NUM_CANDIDATES = 4;
 const ACCEPT_REWARDS = "ACCEPT_REWARDS";
+/**
+ * event triggered by the government for the candidates to propose their blocks
+ * @type {string}
+ */
 const PROPOSE_BLOCK = "PROPOSE_BLOCK";
+/**
+ * event triggered by the gov for the winner candidated to append its block to the blockchain
+ * and braodcast after that.
+ * @type {string}
+ */
 const COMMIT_BLOCK = "COMMIT_BLOCK";
+/**
+ * event triggered by winner delegated to braodcast their block to everyone.
+ */
 const BROADCAST_COMMITED_BLOCK = "BROADCAST_COMMITED_BLOCK";
+/**
+ * event broadcasted for delegators updated account balances.
+ * @type {string} 
+ */
 const GIVE_REWARDS = "GIVE_REWARDS";
+/**
+ * event broadcasted by government for new voting round.
+ * @type {string} 
+ */
+const NEW_VOTING_ROUND = "NEW_VOTING_ROUND";
+/**
+ * event gets triggered by client to cast their votes to the government.
+ * @type {string}
+ */
+const ACCEPT_VOTES = "ACCEPT_VOTES";
+/**
+ * the minimum proportion of votes before determining a winner.
+ * this number should be int the range 0 < x <= 1.0
+ * @type {float} 
+ */
+const EFFECTIVE_VOTES = 1.0;
+const NUM_CANDIDATES = 4;
 const TAX = 0.09;
 const FEES = 0.001;
 
@@ -24,10 +55,10 @@ class Government extends EventEmitter
         this.accounts = {}; // account balances in sys
         this.candidateBlocks = {}; // candidate: block holds the top 4 candidate and eventually thier blocks.
         this.numVoters = numVoters; // acts like the num of entities who can vote.
+        this.voted = 0; // holds the number of people voted so far in a specific voting round.
         this.on(ACCEPT_VOTES, this.acceptVote);
         this.on(PROPOSE_BLOCK, this.accumalateBlock); 
         this.on(BROADCAST_COMMITED_BLOCK, this.updateAccounts);
-        this.on(COMMIT_BLOCK, this.broadcast(BROADCAST_COMMITED_BLOCK, this.block));
         this.on(GIVE_REWARDS, this.distributeRewards());
     }
 
@@ -43,12 +74,20 @@ class Government extends EventEmitter
     }
     /**
      * Accepting votes from all the clients and adding it to the delegate map.
-     * @param {*} name Name of the delegate. delegate[name] equates to the vote count.
+     * after enough votes have been accumalated, trriger *determineCandidates*
+     * @param {Object} vote the vote Object casten by the client.
+     * @param {string} vote.name Name of the delegate. delegate[name] equates to the vote count.
      */
     acceptVote({name})
     {
         if(!this.delegates[name]) throw "delegate not found";
         this.delegates[name]++;
+        this.voted++;
+        // if we met the throushold, find the winner.
+        if(this.voted >= Math.floor(this.numVoters * EFFECTIVE_VOTES))
+        {
+            this.determineCandidates();
+        }
     }
 
     /**
@@ -58,10 +97,13 @@ class Government extends EventEmitter
     {
         // choose a delegate at random
         let winner = Object.keys(this.candidateBlocks)[Math.random()*100 %4];
+        let blockToBeCommited = this.candidateBlocks[winner];
+        if(!blockToBeCommited) throw "there is a problem in finding the winning block. it is unintialized.";
+        // initialize the commiter to indicate the entity to add the block.
+        blockToBeCommited.commiter = winner;
         log(`${winner} is the winner to commit the new blcok`);
-        return winner;
         // TODO find a way to broadcast to winner only.
-        this.broadcast(COMMIT_BLOCK, this.block); //this might change.
+        this.broadcast(COMMIT_BLOCK, blockToBeCommited); 
     }
 
     /**
@@ -85,6 +127,8 @@ class Government extends EventEmitter
         // after finding the top four, ask to propose a block.
         // TODO: find a way to broadcast to candidates only not everyone on the network.
         this.broadcast(PROPOSE_BLOCK);
+        // reset the num of client voted for the next round.
+        this.voted = 0;
     }
 
     /**
@@ -114,11 +158,14 @@ class Government extends EventEmitter
      * this function is used to kick start the election rounds in the system.
      * it works by setting time intervals to boardcast event for users to cast thier votes every 5 seconds.
      * Along with the broadcasted event, the government sends the list of the delegates that people can choose from.
+     * 
+     * For the pupose of this project, every call to this function will cause one round of voting to be triggered.
      */
     startVotingRoundes()
     {
         let listOfdelegates = Object.keys(this.delegates);
-        setInterval(() => this.broadcast(NEW_VOTING_ROUND, listOfdelegates), 5);
+        // setInterval(() => this.broadcast(NEW_VOTING_ROUND, listOfdelegates), 5);
+        this.broadcast(NEW_VOTING_ROUND, listOfdelegates);
     }
 
     /**
@@ -134,23 +181,31 @@ class Government extends EventEmitter
 
         // then take one treansaction at a time and adjust accounts accordingly.
         // keep track of rewards, and tax too
-        let tax = 0, rewards = 0;
-        // block.transactions.foreach.....
-
+        let taxes = 0, rewards = 0;
+        let blockObj = Block.deserialize(block);
+        // move money around.
+        blockObj.getNumTransactions().forEach( ({tax, fees, amount, to, from})  => {
+            // here some transaction verification could happen.
+            // like making sure that the taxes and rewards are of the right proportions.
+            taxes += tax;
+            rewards += fees;
+            this.accounts[from] -= amount;
+            this.accounts[to] += amount;
+        });
+        this.accounts['gov'] += taxes;
+        log(`added ${taxes} coins from taxes.`);
+        // updating the delegate account with the rewards
+        // potential problem here is should we pay all the candidates
+        // we can solve it by only rewarding the candidates who submited a block.
+        // But we are assuming honest candidates so far.
+        Object.keys(this.candidateBlocks).forEach( candidateName => this.accounts[candidateName] += rewards / NUM_CANDIDATES);
 
         // notify the delegates about the new balances with the accounts.
         this.broadcast(ACCEPT_REWARDS, this.accounts);
-    }
-    
-    /**
-     * Calculating the rewards and distributing it to each delegate equally. 
-     */
-    distributeRewards(){
-        let rewards = FEES * Block.getNumTransactions();
-        let rew = rewards / NUM_CANDIDATES;
-        this.broadcast(GIVE_REWARDS, rew);
-    }
 
+        // empty the candidates block obj for the next voting round.
+        this.candidateBlocks = {};
+    }
     
     log(s)
     {
@@ -161,5 +216,13 @@ class Government extends EventEmitter
 
 module.exports = {
     TAX: TAX,
-    FEES: FEES
+    FEES: FEES,
+    ACCEPT_VOTES: ACCEPT_VOTES,
+    NEW_VOTING_ROUND: NEW_VOTING_ROUND,
+    ACCEPT_REWARDS: ACCEPT_REWARDS,
+    PROPOSE_BLOCK: PROPOSE_BLOCK,
+    COMMIT_BLOCK: COMMIT_BLOCK,
+    BROADCAST_COMMITED_BLOCK: BROADCAST_COMMITED_BLOCK,
+    GIVE_REWARDS: GIVE_REWARDS,
+    NEW_VOTING_ROUND: NEW_VOTING_ROUND,
 }
